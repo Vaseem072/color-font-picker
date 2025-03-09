af6100a7af05894da8cc44b0a907183ad4559a78
Vaseem Ahamad <134836138+Vaseem072@users.noreply.github.com>
2025-03-10 02:01:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function changeColor(color){
+    const pro=document.getElementById("head")
+    pro.style.color=color;
+}
+
+
+function backGroundColor(color){
+    const pro=document.getElementById("head")
+    pro.style.backgroundColor=color;
+}
+
+
+function changeFont(font){
+    const pro=document.getElementById("head")
+    pro.style.fontFamily=font;
+}
